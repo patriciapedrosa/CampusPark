9c4a67ce3d518b50b88d4e2a149b906c0062f42a
Roda <tania_roda@outlook.pt>
2018-11-27 18:03:26 +0000

    Rest_database

---
+++ SmartPark/Models/Park.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartPark.Models
+{
+    public class Park
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Number_Spots { get; set; }
+        public DateTime Operating_Hours { get; set; }
+        public int Special_Spots { get; set; }
+    }
+}
---
+++ SmartPark/Models/Spot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartPark.Models
+{
+    public class Spot
+    {
+        public string Id { get; set; }
+        public string Location { get; set; }
+        public string Status { get; set; }
+        public DateTime Time_Status { get; set; }
+        public Boolean Status_Baterry { get; set; }
+        public int Park_Id { get; set; }
+    }
+}
